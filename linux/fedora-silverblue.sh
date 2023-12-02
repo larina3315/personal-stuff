eef6cf36e9aa93cda8c32703bf4c5c6c6afd4c3c
larina3315 <138502262+larina3315@users.noreply.github.com>
2023-12-02 16:26:48 +0600

    Update fedora-silverblue.sh

--- linux/fedora-silverblue.sh
+++ linux/fedora-silverblue.sh
@@ -12,3 +12,6 @@ flatpak install --user org.mozilla.firefox org.mozilla.Thunderbird org.libreoffi
 flatpak install --user org.gnome.Calculator org.gnome.Calendar org.gnome.Snapshot org.gnome.clocks \
  org.gnome.Contacts org.gnome.Evince org.gnome.FontManager org.gnome.font-viewer \
  org.gnome.Logs org.gnome.TextEditor org.gnome.Weather
+
+#GNOME Others
+flatpak install --user com.github.tchx84.Flatseal net.nokyan.Resources
