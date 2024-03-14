a43497bb8aee976b09bcb0f70ae8a169c18c5dea
larina3315 <138502262+larina3315@users.noreply.github.com>
2024-03-15 01:20:14 +0600

    Update fedora-silverblue.sh

--- linux/Scripts/fedora-silverblue.sh
+++ linux/Scripts/fedora-silverblue.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+# rpm-ostree
+# ##########
+# rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/silverblue-main
+# rpm-ostree rebase ostree-image-signed:docker://ghcr.io/ublue-os/silverblue-main:39
+# rpm-ostree rebase ostree-image-signed:docker://ghcr.io/ublue-os/silverblue-nvidia:39
+rpm-ostree override remove firefox-langpacks firefox gnome-software-rpm-ostree
+rpm-ostree install fish gparted langpacks-en langpacks-bn epson-inkjet-printer-escpr <other files>
+
 # Delete Flatpak repos
 flatpak remove --all
 flatpak remote-delete fedora
@@ -14,13 +22,19 @@ flatpak remote-add --if-not-exists --user flathub-beta https://dl.flathub.org/be
 flatpak install -y --system flathub org.mozilla.firefox org.mozilla.Thunderbird org.libreoffice.LibreOffice org.localsend.localsend_app
 
 #GNOME Apps
-flatpak install -y --user flathub org.gnome.Calculator org.gnome.Calendar org.gnome.Snapshot org.gnome.clocks org.gnome.Contacts org.gnome.Evince org.gnome.font-viewer org.gnome.Logs org.gnome.Loupe org.gnome.TextEditor org.gnome.Weather com.github.tchx84.Flatseal net.nokyan.Resources com.mattjakeman.ExtensionManager
+flatpak install -y --user flathub org.gnome.Calculator org.gnome.Calendar org.gnome.Snapshot org.gnome.clocks org.gnome.Contacts org.gnome.Evince org.gnome.font-viewer org.gnome.Logs org.gnome.Loupe org.gnome.TextEditor org.gnome.Weather com.github.tchx84.Flatseal net.nokyan.Resources com.mattjakeman.ExtensionManager io.github.flattool.Warehouse 
 
 # Toolbox
-toolbox create -y devbox
-toolbox run -c devbox sh -c 'echo "max_parallel_downloads=20" | sudo tee -a /etc/dnf/dnf.conf'
-toolbox run -c devbox sh -c 'sudo dnf update -y && sudo dnf install -y fish rustc cargo rust-analyzer helix'
+# toolbox create -y devbox
+# toolbox run -c devbox sh -c 'echo "max_parallel_downloads=20" | sudo tee -a /etc/dnf/dnf.conf'
+# toolbox run -c devbox sh -c 'sudo dnf update -y && sudo dnf install -y fish rustc cargo rust-analyzer helix'
 
-# rpm-ostree
-rpm-ostree cancel && rpm-ostree upgrade && rpm-ostree override remove firefox-langpacks firefox
-rpm-ostree install fish langpacks-en langpacks-bn epson-inkjet-printer-escpr
+#for Distrobox
+# distrobox create -i fedora -n fedora-template
+# distrobox enter fedora-template
+# echo "max_parallel_downloads=20" | sudo tee -a /etc/dnf/dnf.conf
+# sudo dnf update -y && sudo dnf install -y fish
+# distrobox create --clone fedora-template devbox
+# distrobox create --clone fedora-template shitbox
+
+# chsh -s /usr/bin/fish
